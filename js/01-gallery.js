a9205325a520aa621acf2065ac1b84168c99b6e3
anastasia <anastasia@emea-m-5241.speedport.ip>
2023-01-04 15:50:15 +0100

    completed task 1

---
+++ js/01-gallery.js
@@ -0,0 +1,32 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const gallery = document.querySelector('.gallery');
+
+gallery.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(event) {
+    event.preventDefault();
+    let imageSrc = event.target.dataset.source;
+    if (imageSrc !== undefined) {
+        const instance = basicLightbox.create(`
+        <div class="modal">
+            <img src="${imageSrc}"
+                width="1280" height="855" />
+        </div>
+    `, {
+            onShow: (instance) => {
+            document.addEventListener('keydown', onKeyDown);
+
+            function onKeyDown(event) {
+                if (event.key === 'Escape') {
+                    instance.close();
+                }
+            }
+            }
+        });
+
+    instance.show();
+    };
+};
+
