9fbefc95934c9dad784361704f7603906a8999f5
anastasia <anastasia@emea-m-5241.speedport.ip>
2023-01-04 18:31:38 +0100

    completed task 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -27,6 +27,6 @@ function onGalleryClick(event) {
         });
 
     instance.show();
-    };
+    };``
 };
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,9 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+});
 console.log(galleryItems);
