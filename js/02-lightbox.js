598fc4b4c33edf0a5e5565b6aed4a73fddd06093
anastasia <anastasia@emea-m-5241.speedport.ip>
2023-01-09 13:53:16 +0100

    added markup with js, deleted markup in html, completed task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,9 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const gallery = new SimpleLightbox('.gallery a', {
+const galleryEl = document.querySelector('.gallery');
+
+const galleryMarkUp = galleryItems
+    .map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+                    <img class="gallery__image" src="${preview}" alt="${description}">
+                </a>
+`;
+    }).join("");
+
+galleryEl.insertAdjacentHTML("afterbegin", galleryMarkUp);
+galleryEl.addEventListener('click', onImgClick);
+
+function onImgClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+};
+
+new SimpleLightbox('.gallery a', {
     captions: true,
     captionsData: "alt",
-    captionDelay: 250,
+    captionDelay: 250
 });
-console.log(galleryItems);
